807deb80f30f291db95bf9f2e3b61cd3c932dd62
afedotov <a.fedotov@quickresto.org>
2018-07-27 16:15:46 +0500

    1

--- hello.py
+++ hello.py
@@ -1,16 +1,10 @@
-#!/usr/bin/env python3
-
 import requests
 import json
- 
-print("Content-type: text/html")
+
+r = requests.get("https://api.github.com/repos/nafanz/nafanz.github.io)
+
+commits = r.json()
+
+for item in commits:
+print(item['author'])
 print()
-print("<h1>Hello world!</h1>")
-
-r = requests.get("https://api.github.com/repos/nafanz/nafanz.github.io")
-        
-repos = r.json()
-
-test
-repos['updated_at']
-print(repos['updated_at'])
