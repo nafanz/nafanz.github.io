7c708c658758a1070c4c1a37ce11bee6324e1d9c
nafanz <nafanz@mail.ru>
2025-06-30 16:14:23 +0500

    Получаю IP адрес ресурса

--- spreadsheet_check.py
+++ spreadsheet_check.py
@@ -1,5 +1,8 @@
 import json
+import socket
 import requests
+
+user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/138.0.0.0 Safari/537.36'
 
 with open('spreadsheet.json') as file:
     spreadsheet = json.loads(file.read())
@@ -7,11 +10,16 @@ with open('spreadsheet.json') as file:
 for item in spreadsheet:
     name = item['Name'].split("'")[1]
     print(name)
+
+    try:
+        name_to_ip = socket.gethostbyname(name.split("//")[1])
+        print(name_to_ip)
+    except socket.gaierror as error:
+        print(error)
+
     bucket = requests.get(
         url=name,
-        headers={
-            'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/136.0.0.0 Safari/537.36'
-        }
+        headers={'User-Agent': user_agent}
     )
 
     print(bucket.url)
